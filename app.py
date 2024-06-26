ea78dc219f9534f195b2c2b6c4bcf87ec331d351
Balram <balramt02@gmail.com>
2024-06-26 11:57:11 +0200

    Add gitignore and app

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from dotenv import load_dotenv
+
+load_dotenv() ## load all the environment variables from .env
 import streamlit as st
 import os
 from PIL import Image
@@ -6,6 +8,18 @@ import google.generativeai as genai
 
 genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
+## Function to load Gemini Pro Version
+model = genai.GenerativeModel('gemini_pro_version')
 
-model = genai.GenerativeModel('gemini_pro_version'
-                              )
+def get_gemini_response(input,image,propmpt):
+    response = model.generate_content([input,image[0],prompt])
+    return response.text
+
+
+
+## Intiallize our streamlit app
+
+st.set_page_config(page_title= "Multi Language Invoice Extractor)
+st.
+
+
